ccdb7cac994bb1fe8002546c77f30d7f242d8e68
Lindstorm <ethan.godden@gmail.com>
2019-10-26 19:26:32 -0700

    Updated sample connection file

--- FdbApi/src/LocalDatabaseConnection.java
+++ FdbApi/src/LocalDatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class LocalDatabaseConnection {
     public static void main(String[] args) {
         //Add hostname and portnumber to url
-        String url = "jdbc:sqlserver://MYSURFACE; databaseName=FDB;integratedSecurity=true";
+        String url = "jdbc:sqlserver://localhost; databaseName=FDB;integratedSecurity=true";
         try {
             Connection connection = DriverManager.getConnection(url);
         } catch (SQLException  e) {
