e25e706031718b13a96e79f2e6fefbd184b38d36
Lindstorm <ethan.godden@gmail.com>
2019-11-25 11:43:33 -0800

    Reorganized files

--- src/main/java/Prescriber/FdbPrescriber.java
+++ src/main/java/Prescriber/FdbPrescriber.java
@@ -1,13 +1,17 @@
 package Prescriber;
 
 
+import Info.Allergy;
+import Info.Drug;
+import Info.Food;
+import Info.Patient;
+
 import java.util.List;
 
 /**
  * A implementation of {@link Prescriber} using the FDB database
  */
 class FdbPrescriber implements Prescriber {
-
 
     @Override
     public List<Drug> queryDrugs(String prefix) {
