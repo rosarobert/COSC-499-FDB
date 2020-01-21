40b781f1e5753e8e2c86bbcd3f38efad2906cca9
lindstrum <ethan.godden@gmail.com>
2020-01-20 19:27:28 -0800

    Redid allergic reaction to include only displayable info

--- src/main/java/Info/AllergyInteraction.java
+++ src/main/java/Info/AllergyInteraction.java
@@ -1,55 +1,56 @@
 package Info;
 
+import org.apache.commons.lang3.Validate;
+
 /**
- * Container class for storing allergy information
+ * Container class for storing display infomation about a allergy-drug
+ * interaction
  */
 public class AllergyInteraction {
-    //Drug causing the harmful allergy interaction
+    // Drug causing the allergy
     private final Drug DRUG_INTERACTING;
-    //DAM_ALRGN_GRP in relation RDAMGHC0 or DAM_ALRGN_XSENSE in relation RDAMXHC0
-    private final int DRUG_ALLERGY_INTERACTION_CODE;
-    //DAM_ALRGN_GRP_DESC in relation RDAMAGD1 or DAM_ALRGN_XSENSE_DESC in relation RDAMCSD1
-    private final String DRUG_ALLERGY_INTERACTION_RESULT;
+    // Description of the allergy interaction
+    private final String ALLERGY_INTERACTION_RESULT;
 
     private AllergyInteraction(AllergyInteractionBuilder builder) {
-        DRUG_INTERACTING = builder.DRUG_INTERACTING;
-        DRUG_ALLERGY_INTERACTION_CODE = builder.drugAllergyInteractionCode;
-        DRUG_ALLERGY_INTERACTION_RESULT = builder.drugAllergyInteractionResult;
+        DRUG_INTERACTING = builder.drugInteracting;
+        ALLERGY_INTERACTION_RESULT = builder.allergyInteractionResult;
     }
 
     public Drug getInteractingDrug() {
         return DRUG_INTERACTING;
     }
 
-    public int getDrugAllergyInteractionCode() {
-        return DRUG_ALLERGY_INTERACTION_CODE;
+    public String getAllergyInteractionResult() {
+        return ALLERGY_INTERACTION_RESULT;
     }
 
-    public String getDrugAllergyInteractionResult() {
-        return DRUG_ALLERGY_INTERACTION_RESULT;
-    }
-
-
+    /**
+     * Class for building a AllergyInteraction instance
+     * <p>
+     * Note: This builder class is implemented similar to the examples in Chapter 2
+     * Item 2 of Effective Java, 3rd Edition, by Joshua Bloch
+     */
     public static class AllergyInteractionBuilder {
-        private final Drug DRUG_INTERACTING;
-        private int drugAllergyInteractionCode;
-        private String drugAllergyInteractionResult;
+        private Drug drugInteracting;
+        private String allergyInteractionResult;
 
-        public AllergyInteractionBuilder(Drug drug) {
-            DRUG_INTERACTING = drug;
-        }
-
-        public AllergyInteractionBuilder setDrugAllergyInteractionCode(int drugAllergyInteractionCode) {
-            this.drugAllergyInteractionCode = drugAllergyInteractionCode;
+        public AllergyInteractionBuilder setDrugInteracting(Drug drugInteracting) {
+            Validate.notNull(drugInteracting, "The drug causing an allergic reaction cannot be null");
+            this.drugInteracting = drugInteracting;
             return this;
         }
 
-        public AllergyInteractionBuilder setDrugAllergyInteractionResult(String drugAllergyInteractionResult) {
-            this.drugAllergyInteractionResult = drugAllergyInteractionResult;
+        public AllergyInteractionBuilder setAllergyInteractionResult(String allergyInteractionResult) {
+            Validate.notNull(allergyInteractionResult, "The result of an allergic reaction cannot be null");
+            Validate.notEmpty(allergyInteractionResult, "The result of an allergic reaction cannot be an empty.");
+            this.allergyInteractionResult = allergyInteractionResult;
             return this;
         }
 
         public AllergyInteraction buildAllergyInteraction() {
+            Validate.notNull(drugInteracting, "Cannot build an AllergyInteraction without a interacting drug");
+            Validate.notNull(allergyInteractionResult, "Cannot build AllergyInteraction without a result.");
             return new AllergyInteraction(this);
         }
     }
