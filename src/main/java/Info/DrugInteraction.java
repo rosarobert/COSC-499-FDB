7320ae23250025f6cf59c36617a25678be359cb2
lindstrum <ethan.godden@gmail.com>
2020-01-24 13:29:04 -0800

    Added comments and refactored some names in the Info objects

--- src/main/java/Info/DrugInteraction.java
+++ src/main/java/Info/DrugInteraction.java
@@ -1,33 +1,33 @@
 package Info;
 
 import org.apache.commons.lang3.Validate;
-
+//TODO: Could added a Prescriber as a property 
 /**
- * A object describing some interaction with a specific drug. This interaction
- * is usually a harmful interaction like a allergic or food interaction.
+ * A object describing some interaction with a specific drug attempting to be
+ * prescribed by a {@link Prescriber}.
  */
 public class DrugInteraction {
 
-    private final Drug DRUG_PRESCRIBED;// Drug causing the interaction
+    private final Drug DRUG_BEING_PRESCRIBED;// Drug causing the interaction
     private final InteractionType TYPE_OF_INTERACTION;
     private final String INTERACTION_DESCRIPTION;
 
-    DrugInteraction(Drug drugPrescribed, InteractionType typeOfInteraction, String interactionDescription) {
-        Validate.notNull(drugPrescribed, "The a drug in a drug interaction cannot be null.");
+    DrugInteraction(Drug drugBeingPrescribed, InteractionType typeOfInteraction, String interactionDescription) {
+        Validate.notNull(drugBeingPrescribed, "The a drug in a drug interaction cannot be null.");
         Validate.notNull(typeOfInteraction, "The type of a drug interaction cannot be null.");
         Validate.notEmpty(interactionDescription, "The description of a drug interaction cannot be empty or null.");
 
-        DRUG_PRESCRIBED = drugPrescribed;
+        DRUG_BEING_PRESCRIBED = drugBeingPrescribed;
         TYPE_OF_INTERACTION = typeOfInteraction;
         INTERACTION_DESCRIPTION = interactionDescription;
     }
 
     /**
      * Creates a drug interaction corresponding to a harmful food-drug interaction
-     * using the FDB database
+     * using properties from the FDB database
      * 
-     * @param drugInteracting            Drug causing the harmful reaction with the
-     *                                   food.
+     * @param drugBeingPrescribed        Drug that is being prescribed that causes a
+     *                                   harmful reaction with the food.
      * @param foodInteractionDescription A description of what reaction occurs with
      *                                   the food. It is the property RESULT in the
      *                                   relation RDIMMA0
@@ -35,29 +35,28 @@ public class DrugInteraction {
      * @throws IllegalStateException if the description is the empty string
      */
 
-    public static final DrugInteraction createFdbFoodInteraction(Drug drugPrescribed, String foodInteractionDescription) {
-        return new DrugInteraction(drugPrescribed, InteractionType.DRUG_TO_FOOD, foodInteractionDescription);
+    public static final DrugInteraction createFdbFoodInteraction(Drug drugBeingPrescribed, String foodInteractionDescription) {
+        return new DrugInteraction(drugBeingPrescribed, InteractionType.DRUG_TO_FOOD, foodInteractionDescription);
     }
 
     /**
      * Creates a drug interaction corresponding to a harmful drug-allergy
-     * interaction using the FDB database
+     * interaction using properties from the FDB database
      * 
-     * @param drugInteracting               Drug causing the harmful allergic
-     *                                      reaction
+     * @param drugBeingPrescribed Drug causing the harmful allergic reaction
      * @throws NullPointerException  if the drug or description is null
      * @throws IllegalStateException if the description is the empty string
      */
-    public static final DrugInteraction createFdbAllergyInteraction(Drug drugPrescribed) {
-        String description = "The patient is allergic to a ingredient in \"" + drugPrescribed.getDisplayName() + "\"";
-        return new DrugInteraction(drugPrescribed, InteractionType.DRUG_TO_ALLERGY, description);
+    public static final DrugInteraction createFdbAllergyInteraction(Drug drugBeingPrescribed) {
+        String description = "Patient is allergic to a ingredient in \"" + drugBeingPrescribed.getDisplayName() + "\"";
+        return new DrugInteraction(drugBeingPrescribed, InteractionType.DRUG_TO_ALLERGY, description);
     }
 
     /**
      * @return The drug responsible for the interaction
      */
-    public final Drug getDrugPrescribed() {
-        return DRUG_PRESCRIBED;
+    public final Drug getDrugBeingPrescribed() {
+        return DRUG_BEING_PRESCRIBED;
     }
 
     public final InteractionType getInteractionType() {
--- src/main/java/Info/DrugToDrugInteraction.java
+++ src/main/java/Info/DrugToDrugInteraction.java
@@ -1,18 +1,39 @@
 package Info;
 
+/**
+ * A container class for storing information about harmful interactions between
+ * a drug already prescribed (DRUG_ALREADY_PRESCRIBED)  by a {@link Prescriber}
+ * and a drug attempting to be prescribed by a Prescriber .
+ * 
+ * Every DrugToDrug interaction involves a drug already prescribed
+ * (DRUG_PRESENT) and a drug begin added (DRUG_BEING_PRESCRIBED in superclass).
+ * 
+ * The reason for this is to model something that is being prescribed to a
+ * patient a drug; the patient will already have a set of drugs prescribed and a
+ * doctor may want to add a new drug. This class is for storing information
+ * about harmful interactions that may occur when prescribing a new drug
+ */
 public class DrugToDrugInteraction extends DrugInteraction {
-    private final Drug DRUG_PRESENT;
+    private final Drug DRUG_ALREADY_PRESCRIBED;
 
-    DrugToDrugInteraction(Drug drugPrescribed, Drug drugPresent, String description) {
-        super(drugPrescribed, InteractionType.DRUG_TO_DRUG, description);
-        DRUG_PRESENT = drugPresent;
+    /**
+     * Creates a object that described a harmful interaction between two drugs.
+     * 
+     * @param drugBeingPrescribed drug being prescribed that causes a harmful
+     *                            reaction with a drug already prescribed
+     * @param drugPresent         drug that causes a harmful reaction with the drug begin prescribed
+     * @param description
+     */
+    private DrugToDrugInteraction(Drug drugBeingPrescribed, Drug drugAlreadyPrescribed, String description) {
+        super(drugBeingPrescribed, InteractionType.DRUG_TO_DRUG, description);
+        DRUG_ALREADY_PRESCRIBED = drugAlreadyPrescribed;
     }
 
     public static DrugToDrugInteraction createFdbDrugToDrugInteraction(Drug drugPrescribed, Drug drugPresent, String description) {
         return new DrugToDrugInteraction(drugPrescribed, drugPresent, description);
     }
 
-    public Drug getDrugPresent()  {
-        return DRUG_PRESENT;
+    public Drug getDrugAlreadyPrescribed() {
+        return DRUG_ALREADY_PRESCRIBED;
     }
 }
