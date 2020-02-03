0fc5224d6ecab2970cfb33476a19f357350b0c14
lindstrum <ethan.godden@gmail.com>
2020-02-02 16:25:53 -0800

    Changed connection construction because git was not ignoring config file

--- src/main/java/Prescriber/ConnectionConfiguration.java
+++ src/main/java/Prescriber/ConnectionConfiguration.java
@@ -1,45 +1,81 @@
 package Prescriber;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
 /**
- * Class for determining how to construct JDBC connection for FdbPrescriber (but
- * could be used in other implementations)
+ * Class for constructing a JDBC connection from different systems.
  * 
  * This is so the poor soul who has to use linux can run the tests.
  * 
- * Any changes to this class will be ignored by git. This means everyone will be
- * able to customize this class to their own needs. We then can all use this one
- * class to construct the connection even with everyone's settings being
- * different
+ * The class will create a connection from the configuration in
+ * resources/databaseConnection.json if it exists. If it doesn't, creates a the
+ * file databaseConnection.json in resources from the default settings below,
+ * and uses those default settings to create the connection
+ * 
+ * Note, no one should change this class. If you want to change your connection
+ * settings, change them in resources/databaseConnection.json
  * 
  * Another solution to this problem is everyone have the same sa password for
- * SQL Server
+ * SQL Server, but I did not want to have to bring that up since everyone uses
+ * integrated security
  */
 final class ConnectionConfiguration {
-    private static final String SERVER_NAME = "localhost";
-    private static final String DATABASE_NAME = "FDB";
-    private static final boolean USE_INTEGRATED_SECURITY = true;
-    private static final String USER_NAME = "sa";
-    private static final String PASSWORD = "linuxSucks123";
+
+    private static final Gson JSON_SERIALIZER = new Gson();
+
+    @SerializedName("server")
+    private final String SERVER = "localhost";
+
+    @SerializedName("database")
+    private final String DATABASE = "FDB";
+
+    @SerializedName("useIntegratedSecurity")
+    private final boolean USE_INTEGRATED_SECURITY = false;
+
+    @SerializedName("username")
+    private final String USERNAME = "sa";
+
+    @SerializedName("password")
+    private final String PASSWORD = "linuxSucks123";
 
     static final Connection getJdbcConnection() {
-
-        StringBuilder jdbcStringBuilder = new StringBuilder();
-        jdbcStringBuilder.append("jdbc:sqlserver://").append(SERVER_NAME).append(";databaseName=")
-                .append(DATABASE_NAME);
+        ConnectionConfiguration config = new ConnectionConfiguration();
         try {
-            if (USE_INTEGRATED_SECURITY) {
+            File configFile = new File("resources/databaseConnection.json");
+            if (configFile.exists()) {
+                String configFileAsString = Files.readString(configFile.toPath());
+                config = JSON_SERIALIZER.fromJson(configFileAsString, ConnectionConfiguration.class);
+            } else if (configFile.createNewFile()) {
+                PrintWriter writer = new PrintWriter(configFile);
+                writer.write(JSON_SERIALIZER.toJson(config));
+                writer.close();
+            } else {
+                throw new IOException();
+            }
+            StringBuilder jdbcStringBuilder = new StringBuilder();
+            jdbcStringBuilder.append("jdbc:sqlserver://")
+                             .append(config.SERVER)
+                             .append(";databaseName=")
+                             .append(config.DATABASE);
+            if (config.USE_INTEGRATED_SECURITY) {
                 jdbcStringBuilder.append(";integratedSecurity=true");
                 return DriverManager.getConnection(jdbcStringBuilder.toString());
             } else {
-                return DriverManager.getConnection(jdbcStringBuilder.toString(), USER_NAME, PASSWORD);
+                return DriverManager.getConnection(jdbcStringBuilder.toString(), config.USERNAME, config.PASSWORD);
             }
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not configure file");
         } catch (SQLException e) {
-            throw new IllegalStateException("Could not connect to " + DATABASE_NAME + " via " + SERVER_NAME);
+            throw new IllegalStateException("Could not connect to " + config.DATABASE + " via " + config.SERVER);
         }
     }
-
 }
