14285c98752443f5a8dcbe4414b0e9c10544e3b1
lindstrum <ethan.godden@gmail.com>
2020-04-19 22:35:16 -0700

    Performance tests done for query drugs

--- src/test/java/Prescriber/PerformanceTest.java
+++ src/test/java/Prescriber/PerformanceTest.java
@@ -2,14 +2,34 @@ package Prescriber;
 
 public class PerformanceTest {
 
-    Prescriber unoptimizedPrescriber = Prescriber.createFdbPrescriberUnoptimized();
-    Prescriber parallelPrescriber = Prescriber.createFdbPrescriberParallel();
-    Prescriber pagePrescriber = Prescriber.createFdbPrescriberPage();
+    private static final Prescriber UNOPTIMIZED_PRESCRIBER = Prescriber.createFdbPrescriberUnoptimized();
+    private static final Prescriber PARALLEL_PRESCRIBER = Prescriber.createFdbPrescriberParallel();
+    private static final Prescriber PAGE_PRESCRIBER = Prescriber.createFdbPrescriberPage();
+    private static final Prescriber RELATIONAL_PRESCRIBER = Prescriber.createFdbPrescriberRelational();
+    private static final Prescriber PARALLEL_RELATIONAL_PRESCRIBER = Prescriber.createFdbPrescriberParallelRelation();
+    private static final Prescriber PAGE_RELATIONAL_PRESCRIBER = Prescriber.createFdbPrescriberPageRelation();
 
     public static void main(String[] args) {
-        time("Unoptimized Quer", () -> Prescriber.createFdbPrescriberUnoptimized());
-        time("Parallel", () -> Prescriber.createFdbPrescriberParallel());
+
     }
+
+    private static void timeQueryDrugsWithoutPage(String pattern) {
+        System.out.println("Test Query All Drugs");
+        System.out.println();
+        time("Unoptimized", () -> UNOPTIMIZED_PRESCRIBER.queryDrugs(pattern));
+        time("Pagination", () -> PAGE_PRESCRIBER.queryDrugs(pattern));
+        time("Relational", () -> RELATIONAL_PRESCRIBER.queryDrugs(pattern));
+        time("Relational and Pagination", () -> PAGE_RELATIONAL_PRESCRIBER.queryDrugs(pattern));
+    }
+
+    private static void timeQueryDrugsWithPage(String pattern, int page) {
+        System.out.println("Test Query Page of Drugs");
+        System.out.println();
+        time("Unoptimized", () -> UNOPTIMIZED_PRESCRIBER.queryDrugs(pattern, page));
+        time("Pagination", () -> PAGE_PRESCRIBER.queryDrugs(pattern, page));
+    }
+
+
 
     private static void time(String nameOfTest, Runnable operation) {
         try {
